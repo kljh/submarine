39244079717f40cf1f2e3c6506168081a84cd6ad
Claude Cochet <code@main2.fr>
2017-07-21 08:17:00 +0100

    tube dive alt

--- dropbox-oauth.js
+++ dropbox-oauth.js
@@ -141,7 +141,9 @@ function request_upload(opt_oauth_access_token, opt_file_path, opt_file_data, re
         }) 
     ])
     .then(function(api_res) {
-        api_res[1].url;
+        try { 
+            api_res = { url: api_res[1].url, api_res: api_res };
+        } catch(e) {} 
         if (res)
             res.send(api_res);
         else 
